290deeefba30be922a842ddf1871ea117cc1b555
Ytalo Ethaniel <ysa4@aluno.ifal.edu.br>
2023-02-12 17:07:57 +0000

    adicionando conexão com db

---
+++ src/database/connection.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_CONNECTION_URI, {
+  dialect: 'postgres'
+});
+
+exports.connect = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully');
+  } catch (error) {
+    console.log('Unable to connect to database:', error);
+  }
+};
+exports.db = sequelize;
