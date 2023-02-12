d7009ffbbc6be89c9060a661b6e8922356810740
Ytalo Ethaniel <ysa4@aluno.ifal.edu.br>
2023-02-12 22:29:27 +0000

    criando rotas de adicionar categoria e indicados

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,5 +1,56 @@
 const router = require('express').Router();
+const { ValidationError } = require('sequelize');
 
-router.post('/adicionarCategoriaOscar', (req, res, next) => {
-  console.log(req.body);
+const CategoryOscar = require('../models/categoryOscar');
+const NomineeOscar = require('../models/nomineeOscar');
+
+router.post('/adicionarCategoriaOscar', async (req, res, next) => {
+  const body = req.body;
+
+  if (!body.category) {
+    res.status(400);
+    next(new Error("Falta parâmetro 'category'"));
+    return;
+  }
+  await CategoryOscar.create({ category: body.category });
+
+  res.json(await CategoryOscar.findAll());
 });
+
+router.post('/adicionarIndicadoOscar', async (req, res, next) => {
+  try {
+    const body = req.body;
+
+    [
+      'nominated',
+      'film',
+      'category'
+    ].forEach(el => {
+      if (!body[el]) {
+        res.status(400);
+        throw new Error("Falta parâmetro " + el);
+      }
+    });
+
+    const categoryExists = await CategoryOscar.findAll({
+      where: {
+        category: body.category
+      }
+    });
+    if (!categoryExists.length)
+      throw new Error(`Não existe categoria '${body.category}'`);
+
+    const newNominee = await NomineeOscar.create({
+      'nominated': body.nominated,
+      'film': body.film,
+      'CategoryOscarId': categoryExists[0].get('id')
+    });
+    res.send(newNominee);
+  } catch (error) {
+    if (error instanceof ValidationError)
+      res.status(400);
+    next(error);
+  }
+});
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+const createError = require('http-errors');
 
 router.get('/', function(req, res, next) {
   res.json({ msg: 'tudo certo' });
 });
 
-module.exports = router;
+module.exports = (app) => {
+  const admin = require('./admin');
+  router.use(admin);
+
+  app.use(router);
+
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.json({ erro: err.message });
+  });
+};
