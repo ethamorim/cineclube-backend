4f0660996fee3d272629206fc9362408d3c4df1f
Ethan <ysa4@aluno.ifal.edu.br>
2023-03-04 16:22:00 -0300

    sei la

--- src/app.js
+++ src/app.js
@@ -15,26 +15,26 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  allowedHeaders: [
-    'Origin',
-    'X-Requested-With',
-    'Content-Type',
-    'Accept'
-  ],
-  credentials: true
-}));
 // app.use(cookieParser(process.env.SECRET_SESSION_KEY));
 app.use(session({
-  secret: process.env.SECRET_SESSION_KEY,
+  secret: 'secret',
   resave: false,
   saveUninitialized: false,
   cookie: {
     maxAge: 1000 * 60 * 60 * 24,
-    secure: true
+    sameSite: false,
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true
   },
+}));
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'OPTIONS', 'HEAD'],
+  allowedHeaders: [
+    'X-Requested-With',
+    'Content-Type',
+  ],
+  credentials: true
 }));
 app.use(logger('dev'));
 
