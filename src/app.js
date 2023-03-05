e3db45b0579152f8782110187e424bc96b2579c7
Ytalo Ethaniel <ysa4@aluno.ifal.edu.br>
2023-03-04 21:55:45 -0300

    adicionando localhost ao cors

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.use(
 
 app.use(
   cors({
-    origin: ["https://cine-nandowastaken.vercel.app/"],
+    origin: ["https://cine-nandowastaken.vercel.app/", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "PATCH", "HEAD", "OPTIONS"],
     allowedHeaders: ["X-Requested-With", "Content-Type"],
     credentials: true,
