5e3f68d7a791d401ea30c2ee09967cc47e953f25
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2023-03-05 15:16:38 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ app.use(
   cors({
     origin: ["https://cine-nandowastaken.vercel.app/", "http://localhost:5173", "http://127.0.0.1:5173/"],
     methods: ["GET", "POST", "PUT", "PATCH", "HEAD", "OPTIONS"],
-    allowedHeaders: ["X-Requested-With", "Content-Type"],
+    allowedHeaders: ["X-Requested-With", "Content-Type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Accept"],
     credentials: true,
   })
 );
