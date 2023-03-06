b11c4bebfe68f1a47c26b4ccfcc0e2108a063d40
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2023-03-06 17:18:05 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.use(
 );
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', "https://cine-nandowastaken.vercel.app/", "https://cineclube.online/", "https://cineclube.online/login", "https://cineclube.online/register"],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', "https://cine-nandowastaken.vercel.app/", "https://cineclube.online/"],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'HEAD', 'OPTIONS'],
   allowedHeaders: [
     'X-Requested-With',
@@ -46,6 +46,8 @@ app.use(cors({
   ],
   credentials: true,
 }));
+
+app.use(cors());
 
 app.use(logger('dev'));
 
